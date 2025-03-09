9ded72329d0006338c265f12518e082d1068d946
anya <anyadara@gmail.com>
2025-03-09 14:57:59 +0300

    JAVA  PROJECT

---
+++ OneDrive/Documents/NetBeansProjects/AA/src/oop/Lab2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Msi
+ */
+public class Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+public class
+person l
+private String name;
+public void setname (String n) f
+name=n;
+public String getname () ( return name;
+private int age;
+public void setage (int a) l
+age=a;
+public int getage () / return age;
+private String country;
+public void setcountry (String c) /
+country=c;
+public String getcountry () / return country;    }
+    
+}
---
+++ OneDrive/Documents/NetBeansProjects/AA/src/oop/oop1.java
@@ -0,0 +1,21 @@
+package oop;
+
+public class oop1 {
+private String name;
+public void setname (String n) {
+        
+name=n;}
+public String getname () { 
+        return name;}
+private int age;
+public void setage (int a) {
+age=a;}
+public int getage (){
+      return age;}
+private String country;
+public void setcountry (String c) {
+country=c;}
+public String getcountry () {
+    return country;
+}
+}
